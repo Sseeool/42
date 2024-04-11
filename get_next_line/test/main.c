9218e9d378d3fb50f0e9d5ecea70bb9bac674413
Sseeool <eonseo15@daum.net>
2024-04-11 10:55:56 +0900

    make d files

---
+++ get_next_line/test/main.c
@@ -0,0 +1,39 @@
+#include "get_next_line.c"
+#include "get_next_line.h"
+#include "get_next_line_utils.c"
+
+/*#include "get_next_line_bonus.c"
+#include "get_next_line_bonus.h"
+#include "get_next_line_utils_bonus.c"*/
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int main()
+{
+	int fd1;
+	//int	fd2;
+	char *line;
+
+	fd1 = open("test.txt", O_RDONLY);
+	//fd2 = open("test2.txt", O_RDONLY);
+
+	/*while((line = get_next_line(fd1)))
+	{
+		printf ("%s", line);
+		free(line);
+	}*/
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%s", get_next_line(fd));
+	}
+	close(fd1);
+	/*while((line = get_next_line(fd2)))
+	{
+		printf ("%s", line);
+		free(line);
+	}
+	close(fd2);*/
+	return 0;
+}
